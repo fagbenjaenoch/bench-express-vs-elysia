404951f2569ded5adc63b453841c8dcf77e1ee72
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-11-20 15:36:52 +0100

    modified elysia server return value

--- elysia-app/src/index.ts
+++ elysia-app/src/index.ts
@@ -1,6 +1,8 @@
-import { Elysia } from "elysia";
+import { Elysia} from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", (ctx) => {
+  return {message: "Hello from Bun with Elysia!"};
+}).listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
