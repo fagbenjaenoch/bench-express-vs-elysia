3fd698dd482c6d09fee6a1dc607052732e5158bf
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-11-20 15:19:50 +0100

    wrote a basic server in bun express

--- bun-express-app/index.ts
+++ bun-express-app/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import express from 'express';
+import "dotenv/config";
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Bun with Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
