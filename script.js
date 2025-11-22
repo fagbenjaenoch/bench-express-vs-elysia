25860d0714998ba03b64cccc44a0157851caf217
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-11-22 10:28:51 +0100

    added script to test all endpoints and save their result in a file

---
+++ script.js
@@ -0,0 +1,17 @@
+import { execSync } from "child_process";
+
+const apis = {
+	elysia: "http://localhost:3000",
+	expressWithBun: "http://localhost:3001",
+	express: "http://localhost:3002",
+};
+
+for (const [name, url] of Object.entries(apis)) {
+	console.log(`Running tests for ${url}...`);
+
+	execSync(`BASE_URL=${url} k6 run test.js > result_${name}.txt`, {
+		stdio: "inherit",
+	});
+}
+
+console.log("All tests complete");
