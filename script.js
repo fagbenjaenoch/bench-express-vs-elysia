b7f71cac7268f0025fa5bac3256041984fe2b5bf
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-11-24 14:40:06 +0100

    create result dir on initial launch

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 import { execSync } from "child_process";
+import fs from "node:fs";
 
 const apis = {
 	elysia: "http://localhost:3000",
 	expressWithBun: "http://localhost:3001",
 	express: "http://localhost:3002",
 };
+
+// create results directory
+fs.mkdir("./results", (err) => {
+	console.error(`An error occurred while creating the directory: ${err}`);
+});
 
 for (const [name, url] of Object.entries(apis)) {
 	console.log(`Running tests for ${url}...`);
